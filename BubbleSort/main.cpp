ec70f1f2aa975110c74b3e689de1ceadcec8a134
Adeeb Mohammed <adeeb2358@gmail.com>
2020-01-04 20:31:18 +0900

    bubble sort algorithm

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -6,10 +6,38 @@
 //  Copyright © 2020 adeeb mohammed. All rights reserved.
 //
 
-#include <iostream>
+// bubble sort algorithm
 
+#include <iostream>
+#include <vector>
+// time complexity is o(n2) worst case and best case o(n)
+//space complexity is o(1)
+std::vector<int> bubbleSort(std::vector<int> array) {
+    bool swapDone       = false;
+    int  arrayBoundary  = (int)array.size();
+    int  numOne         = 0;
+    int  numTwo         = 0;
+    do{
+        swapDone   = false;
+        for(int i = 0; i < (arrayBoundary - 1); i++){
+            numOne = array.at(i);
+            numTwo = array.at(i+1);
+            if(numOne > numTwo){
+                array.at(i)   = numTwo;
+                array.at(i+1) = numOne;
+                swapDone      = true;
+            }
+        }
+        arrayBoundary--;
+    }while(swapDone == true);
+    return array;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Program for bubble sort" << std::endl;
+    auto array = {3,9,2,1,10,15,-1};
+    auto result = bubbleSort(array);
+    for(auto it = result.begin(); it != result.end(); ++it){
+        std::cout << *it << " ";
+    }
     return 0;
 }
